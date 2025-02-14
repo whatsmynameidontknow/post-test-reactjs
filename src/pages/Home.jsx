da18d5a565c86c3d43f4c1d53b480d1e0c12b69f
whatsmynameidontknow <akunhantusaya@gmail.com>
2025-02-14 18:26:09 +0700

    refactor: !

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                     <div className="flex flex-column align-items-center justify-content-center gap-3 p-4">
                         <i className="pi pi-home text-5xl text-blue-600"></i>
                         <h1 className="text-3xl md:text-5xl font-bold text-center text-gray-900 m-0">
-                            Welcome Home
+                            Welcome!
                         </h1>
                         <p className="text-xl md:text-2xl text-center text-gray-600 mt-2">
                             Gatau mau diisi apa, biar ga kosong aja
