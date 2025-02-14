a7387dcec970b3e98ea070c56d45c6c44096ca8b
whatsmynameidontknow <akunhantusaya@gmail.com>
2025-02-14 15:04:01 +0700

    feat: navbar active

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,71 @@ import Projects from './pages/Projects';
 
 function App() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const currentPath = location.pathname.toLowerCase();
+
     const navbarItems = [
         {
             label: 'Projects',
             icon: 'pi pi-lightbulb',
             command: () => navigate('/projects'),
+            template: (item) => (
+                <span
+                    className={`p-menuitem-link ${
+                        currentPath === '/projects'
+                            ? 'text-blue-500 font-bold'
+                            : 'text-gray-700'
+                    }`}
+                    onClick={item.command}
+                >
+                    <i className={`${item.icon} mr-2`}></i>
+                    <span>{item.label}</span>
+                </span>
+            ),
         },
         {
             label: 'People',
             icon: 'pi pi-users',
             command: () => navigate('/people'),
+            template: (item) => (
+                <span
+                    className={`p-menuitem-link ${
+                        currentPath === '/people'
+                            ? 'text-blue-500 font-bold'
+                            : 'text-gray-700'
+                    }`}
+                    onClick={item.command}
+                >
+                    <i className={`${item.icon} mr-2`}></i>
+                    <span>{item.label}</span>
+                </span>
+            ),
         },
         {
             label: 'About',
             icon: 'pi pi-info-circle',
             command: () => navigate('/about'),
+            template: (item) => (
+                <span
+                    className={`p-menuitem-link ${
+                        currentPath === '/about'
+                            ? 'text-blue-500 font-bold'
+                            : 'text-gray-700'
+                    }`}
+                    onClick={item.command}
+                >
+                    <i className={`${item.icon} mr-2`}></i>
+                    <span>{item.label}</span>
+                </span>
+            ),
         },
     ];
 
     const VALID_ROUTES = new Set(['/people', '/', '/projects', '/about']);
-    const location = useLocation();
+
     return (
         <>
-            {VALID_ROUTES.has(location.pathname.toLowerCase()) && (
-                <Navbar items={navbarItems} />
-            )}
+            {VALID_ROUTES.has(currentPath) && <Navbar items={navbarItems} />}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/people" element={<People />} />
