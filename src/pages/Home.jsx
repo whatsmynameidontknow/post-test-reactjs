d142233c8dc1be327ce5b69d21d1e3feb5169473
whatsmynameidontknow <akunhantusaya@gmail.com>
2025-02-17 21:34:11 +0700

    style: kurangin padding di home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ import { Card } from 'primereact/card';
 
 export default function Home() {
     return (
-        <div className="px-2 md:px-4 py-4 md:py-8 bg-gray-50 min-h-screen">
+        <div
+            className="flex column align-items-center"
+            style={{
+                minHeight: '80vh',
+            }}
+        >
             <div className="max-w-6xl mx-auto">
                 <Card className="w-full shadow-lg hover:shadow-xl transition-shadow duration-200">
                     <div className="flex flex-column align-items-center justify-content-center gap-3 p-4">
